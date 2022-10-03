ec824bbe145db1a19fcc515d244026c50cfb9c15
user <and.m97987@gmail.com>
2022-10-03 19:53:20 +0400

    Smart Edu hmtl css controller

---
+++ src/main/java/am/itspace/smart_education/controller/AboutController.java
@@ -0,0 +1,13 @@
+package am.itspace.smart_education.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AboutController {
+
+    @GetMapping("/about_us")
+    public String aboutPage(){
+        return "about";
+    }
+}
---
+++ src/main/java/am/itspace/smart_education/controller/TeachersController.java
@@ -0,0 +1,13 @@
+package am.itspace.smart_education.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TeachersController {
+
+    @GetMapping("/teachers")
+    public String teachersPage(){
+        return "teachers";
+    }
+}
