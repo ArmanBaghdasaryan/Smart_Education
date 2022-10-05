fcf61f2b1e1757e3274d653aa710b7b58e1dccee
user <and.m97987@gmail.com>
2022-10-05 17:38:54 +0400

    delete mistakes

--- src/main/java/am/itspace/smart_education/common/repositories/SubscriptionRepository.java
+++
@@ -1,8 +0,0 @@
-package am.itspace.smart_education.common.repositories;
-
-import am.itspace.smart_education.common.entity.Subscription;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
-
-}
