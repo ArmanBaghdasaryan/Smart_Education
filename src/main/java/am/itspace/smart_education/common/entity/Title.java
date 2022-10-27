fc7c035325a854952959cef856b340b49c3007dd
araskhanyanspartak <araskhanyanspartak@gmail.com>
2022-10-28 01:17:19 +0400

    added admin controllers and htmls

--- src/main/java/am/itspace/smart_education/common/entity/Title.java
+++ src/main/java/am/itspace/smart_education/common/entity/Title.java
@@ -1,9 +1,16 @@
 package am.itspace.smart_education.common.entity;
 
 public enum Title {
-    JAVA,
-    JS,
-    C,
-    PHP,
-    PYTHON
+    JAVA("Java"),
+    JS("Java script"),
+    C_PLUS("C++"),
+    C("C"),
+    PHP("Php"),
+    PYTHON("Python");
+
+    private String displayName;
+
+    Title(String displayName) {
+        this.displayName = displayName;
+    }
 }
--- src/main/java/am/itspace/smart_education/controller/admin/AdminAnswersController.java
+++ src/main/java/am/itspace/smart_education/controller/admin/AdminAnswersController.java
@@ -5,49 +5,47 @@ import am.itspace.smart_education.common.service.AnswerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminAnswersController {
 
     private final AnswerService answerService;
 
-    @GetMapping("/admin/answers")
+    @GetMapping("/answers")
     public String answers(ModelMap modelMap) {
         List<Answer> allAnswers = answerService.findAll();
         modelMap.addAttribute("answers", allAnswers);
-        return "answers";
+        return "admin/admin_answers";
     }
 
-    @GetMapping("/admin/answers/add")
+    @GetMapping("/answers/add")
     public String addAnswers() {
         return "addAnswers";
     }
 
 
-    @PostMapping("/admin/answers/add")
+    @PostMapping("/answers/add")
     public String addAnswers(@ModelAttribute Answer answer) {
         answerService.save(answer);
-        return "redirect:/admin/answers";
+        return "redirect:admin/answers";
     }
 
 
-    @GetMapping("/admin/answers/delete")
+    @GetMapping("/answers/delete")
     public String deleteAnswers(@RequestParam("id") int id) {
         answerService.deleteById(id);
-        return "redirect:/admin/answers";
+        return "redirect:admin/answers";
     }
 
-    @GetMapping("/admin/answers/update")
+    @GetMapping("/answers/update")
     public String update(@ModelAttribute Answer answer) {
         answerService.save(answer);
-        return "redirect:/admin/answers";
+        return "redirect:admin/answers";
     }
 
 }
