5455bc72873c07c5f1d96a6793e356c4f63176df
user <and.m97987@gmail.com>
2022-11-12 23:20:44 +0400

    add teachersPage

---
+++ src/main/java/am/itspace/smart_education/controller/web/TeachersPageController.java
@@ -0,0 +1,34 @@
+package am.itspace.smart_education.controller.web;
+
+import am.itspace.smart_education.common.entity.User;
+import am.itspace.smart_education.common.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+public class TeachersPageController {
+
+    private final UserService userService;
+
+    @GetMapping("/teachersPage")
+    public String teachers() {
+        return "web/teachersPage";
+    }
+
+    @GetMapping("/teachers/{id}")
+    public String bookSinglePage(@PathVariable("id") int id, ModelMap modelMap) {
+        Optional<User> byId = userService.findById(id);
+        if (byId.isEmpty()) {
+            return "redirect:/";
+        }
+        modelMap.addAttribute("teachersPage", byId.get());
+        return "web/teachersPage";
+    }
+
+}
