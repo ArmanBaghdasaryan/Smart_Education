6e28e7ccb4ba3c057cdd7fabe6b4170badb8e00e
user <and.m97987@gmail.com>
2022-11-12 23:21:09 +0400

    add teachersPage

--- src/main/java/am/itspace/smart_education/controller/web/TeachersController.java
+++ src/main/java/am/itspace/smart_education/controller/web/TeachersController.java
@@ -1,13 +1,37 @@
 package am.itspace.smart_education.controller.web;
 
+import am.itspace.smart_education.common.entity.User;
+import am.itspace.smart_education.common.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class TeachersController {
+    private final UserService userService;
+   
 
     @GetMapping("/teachers")
-    public String teachersPage(){
+    public String bookSinglePage(ModelMap modelMap) {
+        List<User> allTeacher = userService.findByRoleTeacher();
+        if (allTeacher.isEmpty()) {
+            return "redirect:/";
+        }
+        modelMap.addAttribute("teachers", allTeacher);
         return "web/teachers";
+
+    }
+
+    @GetMapping(value = "/getImage", produces = MediaType.IMAGE_JPEG_VALUE)
+    public @ResponseBody byte[] getImage(@RequestParam("fileName") String fileName) throws IOException {
+        return userService.getUserImage(fileName);
     }
 }
