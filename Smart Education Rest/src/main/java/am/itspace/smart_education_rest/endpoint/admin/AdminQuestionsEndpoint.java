965884695fac0addf923c40800c88fb4a7840a33
user <and.m97987@gmail.com>
2022-12-12 01:58:23 +0400

    fix links

---
+++ Smart Education Rest/src/main/java/am/itspace/smart_education_rest/endpoint/admin/AdminQuestionsEndpoint.java
@@ -0,0 +1,56 @@
+package am.itspace.smart_education_rest.endpoint.admin;
+
+import am.itspace.smart_education_common.dto.QuestionDto;
+import am.itspace.smart_education_common.entity.Question;
+import am.itspace.smart_education_common.mapper.QuestionMapper;
+import am.itspace.smart_education_common.service.QuestionsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/questions")
+public class AdminQuestionsEndpoint {
+    private final QuestionsService questionsService;
+    private final QuestionMapper questionMapper;
+
+    @GetMapping
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        return ResponseEntity.ok(questionsService.findAll());
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<Question> getQuestionId(@PathVariable("id") int id) {
+        return questionsService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Question> createQuestion(@RequestBody QuestionDto questionDto) {
+        questionsService.updateQuestion(questionMapper.map(questionDto));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteQuestionById(@PathVariable("id") int id) {
+        questionsService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Question> updateQuestionById(@PathVariable("id") int id,
+                                                       @RequestBody Question question) {
+        Optional<Question> byId = questionsService.findById(id);
+        if (byId.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+        questionsService.updateQuestion(question);
+        return ResponseEntity.ok(question);
+    }
+}
